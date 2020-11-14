4b204e9d8edf33925a9f22a67b903a47f4c5b3f0
ww2or3ww <w2or3w@gmail.com>
2020-11-14 13:27:52 +0000

    try2merge4xx

--- source/lambda_function.py
+++ source/lambda_function.py
@@ -20,7 +20,7 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-SUPPORTED_TYPE = "main_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,patients:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,patients_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,inspection_persons:d4827176-d887-412a-9344-f84f161786a2,contacts:1b57f2c0-081e-4664-ba28-9cce56d0b314"
+SUPPORTED_TYPE = "main_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,main_summary:92f9ebcd-a3f1-4d5d-899b-d69214294a45,patients:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,patients_summary:5ab47071-3651-457c-ae2b-bfb8fdbe1af1,inspection_persons:d4827176-d887-412a-9344-f84f161786a2,contacts:1b57f2c0-081e-4664-ba28-9cce56d0b314"
 
 def lambda_handler(event, context):
     try:  
